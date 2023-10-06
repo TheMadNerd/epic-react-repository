512c30c8deef5fe77718c213ffd7b041eddf4ea3
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-10-06 11:55:00 +0200

    rect-suspense

---
+++ src-suspense/exercise/01.js
@@ -0,0 +1,45 @@
+// Simple Data-fetching
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
+
+let pokemon
+let pokemonError
+let pokemonPromise = fetchPokemon('pikachu').then(
+  p => (pokemon = p),
+  e => (pokemonError = e),
+)
+
+function PokemonInfo() {
+  if (pokemonError) {
+    throw pokemonError
+  }
+  if (!pokemon) {
+    throw pokemonPromise
+  }
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="pokemon-info-app">
+      <div className="pokemon-info">
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={<div>loading...</div>}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
+      </div>
+    </div>
+  )
+}
+
+export default App
